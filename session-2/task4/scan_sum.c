47df66f89221e4823c516d67bbee549829453327
Harry Hopkinson <63599884+Harry-Hopkinson@users.noreply.github.com>
2026-02-20 10:50:35 +0000

    Complete Task 4

--- session-2/task4/scan_sum.c
+++ session-2/task4/scan_sum.c
@@ -4,9 +4,28 @@
 int main( void ) {
 
     // define suitable data
+    int num1, num2;
+    char operator;
 
     // use scanf to read from the terminal
-    // print the output from scanf and the data values 
+    num1 = scanf("%d %c %d", &num1, &operator, &num2);
+
+    // print the output from scanf and the data values
+    int answer;
+    switch (operator)
+    {
+        case '+': // +
+            answer = num1 + num2;
+        case '-': // -
+            answer = num1 - num2;
+        case '*': // *
+            answer = num1 * num2;
+        case '/': // /
+            if (num2 == 0) answer = 0;
+            else answer = num1 / num2;
+    }
+
+    printf("Answer: %d\n", answer);
 
     return 0;
 }
