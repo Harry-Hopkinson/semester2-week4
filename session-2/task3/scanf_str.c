21c2d36459b2b967051066247baf62fa977c2265
Harry Hopkinson <63599884+Harry-Hopkinson@users.noreply.github.com>
2026-02-20 10:29:47 +0000

    Complete Task 3

--- session-2/task3/scanf_str.c
+++ session-2/task3/scanf_str.c
@@ -4,8 +4,8 @@
 int main( void ) {
     char input[10];
 
-    int count = scanf("%s",input);
-    //int count = scanf("%9s",input);
+    // int count = scanf("%s",input); bad code as it causes a buffer overflow if more than 9 chars
+    int count = scanf("%9s", input); // limit to 9 characters to prevent buffer overflow
 
     printf("Read %d value\n",count);
     printf("Values: %s\n",input);
