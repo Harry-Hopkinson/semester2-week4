9229ea3c78408152463c35eca086824936b1c447
Harry Hopkinson <63599884+Harry-Hopkinson@users.noreply.github.com>
2026-02-20 09:50:36 +0000

    Complete Task 2

--- session-2/task2/string_ops.c
+++ session-2/task2/string_ops.c
@@ -14,10 +14,17 @@ int main( void ) {
     // use printf to verify the following tests
 
     // use 'strcmp' to compare 2 strings - print the return value
+    int result = strcmp(str1, str2);
+    if (result == 0) printf("%s and %s are the same\n", str1, str2);
+    else printf("%s and %s are not the same\n", str1, str2);
 
     // use 'strcat' to concatenate 2 strings - print the resulting string
+    char* concatenated_string = strcat(str1, str2);
+    printf("Concatenating str1 and str2 gives: %s\n", concatenated_string);
 
     // use 'strcpy' to copy a string into str3 - print string 3
+    strcpy(str3, concatenated_string);
+    printf("String 3: %s\n", str3);
 
     return 0;
 }
